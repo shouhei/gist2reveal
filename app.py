5ae30718e3523f3ce40d6005dbfe5e89b0da7385
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-04-04 15:30:56 +0900

    add args

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def md2reveal(id=None):
     r_dict = r.json()
     response = {}
     for data in r_dict["files"].values():
-        response["body"] = pypandoc.convert(data["content"],'revealjs',format='md')
+        response["body"] = pypandoc.convert(data["content"],'revealjs',format='md', extra_args=['--slide-level=2'])
         response["title"] = data['filename']
     return render_template('index.html',title=response['title'], body=response['body'])
 
